8eb6a1836e9355260ec6d78ebe2111caf0013717
Dave Tang <davetingpongtang@gmail.com>
2014-11-08 13:36:54 +0900

    Table of non-coding RNAs

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -f main.acn main.aux main.bbl main.blg main.glsdefs main.ist main.lof main.log main.nlo main.synctex.gz main.toc main.dvi main.acr main.alg main.out
+rm -f main.acn main.aux main.bbl main.blg main.glsdefs main.ist main.lof main.log main.nlo main.synctex.gz main.toc main.dvi main.acr main.alg main.out main.lot
