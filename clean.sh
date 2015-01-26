778b30c0f62629c61a36b8d6c0759450c1760785
Dave Tang <davetingpongtang@gmail.com>
2015-01-26 23:21:38 +0900

    Drafting rebuttal

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -f main.acn main.aux main.bbl main.blg main.glsdefs main.ist main.lof main.nlo main.synctex.gz main.toc main.dvi main.acr main.alg main.out main.lot
+rm -f *.acn *.aux *.bbl *.blg *.glsdefs *.ist *.lof *.nlo *.synctex.gz *.toc *.dvi *.acr *.alg *.out *.lot
